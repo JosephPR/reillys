89a6e9c7f0333b5fbd4b294d558023fc4c45d6a8
JosephPR <reilly4181@gmail.com>
2019-07-08 13:55:15 -0600

    updated our menu items to navigate withRouter higher order component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import HomePage from './Pages/Homepage/homepage'
+import HomePage from './Pages/Homepage/homepage';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+const FunnyPage = () => {
+  return (
+    <div>
+      <h1>Funny Page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/funny' component={FunnyPage} />
+      </Switch>
+
     </div>
   );
 }
--- src/Components/Directory/directory.js
+++ src/Components/Directory/directory.js
@@ -10,36 +10,36 @@ export default class Directory extends Component {
     this.state = {
       sections: [
         {
-          title: 'FUNNY',
+          title: 'Funny',
           imageUrl: 'http://static1.squarespace.com/static/58d9aba3893fc01b3a3e1310/595288fe4c8b036ff814ba34/5af0700f8a922ddd8dd60e89/1553140731386/NEW-Stickers-Squarespace_Artboard+25.jpg',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'funny'
         },
         {
-          title: 'CUTE',
+          title: 'Cute',
           imageUrl: 'https://res.cloudinary.com/teepublic/image/private/s---INt7UTw--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1508896856/production/designs/1997769_1.jpg',
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: 'shop/cute'
         },
         {
-          title: 'LAPTOP',
+          title: 'Laptop',
           imageUrl: 'https://cdn.trendhunterstatic.com/thumbs/Apple-Art-Vinyl-Decals.jpeg',
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: 'shop/laptop'
         },
         {
-          title: 'SKATER',
+          title: 'Skater',
           imageUrl: 'https://i.etsystatic.com/5434622/r/il/3a72cc/1902135439/il_794xN.1902135439_8mh4.jpg',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/womens'
+          linkUrl: 'shop/skater'
         },
         {
-          title: 'RANDOM',
+          title: 'Random',
           imageUrl: 'http://thumb2.zeppy.io/d/l400/pict/183301428287/1000x-vinyl-decal-graffiti-stickers-bomb-for-car-luggage-laptop-skate-waterproof',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/mens'
+          linkUrl: 'shop/random'
         }
       ]
     }
@@ -49,9 +49,9 @@ export default class Directory extends Component {
     return (
       <div className='directory-menu'>
       {
-        this.state.sections.map(({title, imageUrl, size, id}) => {
+        this.state.sections.map(({ id, ...sectionProps }) => {
           return (
-            <MenuItem key={id}  title={title} imageUrl={imageUrl} size={size}/>
+            <MenuItem key={id} {...sectionProps}/>
           )
         })
 
