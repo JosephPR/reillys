c4ed9c646e11f230e49c9b03d1f651359befeecc
JosephPR <reilly4181@gmail.com>
2019-07-13 12:26:56 -0600

    header with shop and contact link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from './Pages/Homepage/homepage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './Pages/Shop/shop'
+import Header from './Components/Header/header'
 import './App.css';
 
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
